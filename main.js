effcac85bcc8874da3e4f6ae779f7f38ffe85047
krystal gates <krystal_sabrina@yahoo.com>
2019-06-20 09:37:04 -0500

    complete exercise

--- main.js
+++ main.js
@@ -5,3 +5,7 @@
 
 
 var integers = [13, 25, 6, 3, 11, 2, 18, 7, 21, 1, 29, 20, 12, 8];
+
+let sortIt = integers.sort((curr, next) => next - curr).filter(num => num < 19).map(num => (num * 1.5) - 1).reduce((curr, next) => curr + next)
+
+console.log(sortIt)
